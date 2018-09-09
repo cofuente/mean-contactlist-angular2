d864dcdd128fb5bdd064d453c393694f675e060f
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-09-09 16:48:01 -0400

    moving it back to body

--- server.js
+++ server.js
@@ -82,11 +82,11 @@ app.post('/api/contacts', function(req, res) {
 })
 
 app.post('/api/events', function(req, res) {
-  console.log(req.body)
-  if (!req.params.challenge) {
+  console.log(req.body.challenge)
+  if (!req.body.challenge) {
     handleError(res, 'no Challege', 400)
   } else {
-    res.status(201).json(req.params.challenge)
+    res.status(200).send(req.body.challenge)
   }
 })
 
